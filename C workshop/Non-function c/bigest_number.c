9f4b0f5925c4be84de89ccaaddfed721c6aa26f6
G.Ganesh <158814955+GokaGaneshFP2@users.noreply.github.com>
2024-02-12 13:42:49 +0530

    Update bigest_number.c

--- C workshop/Non-function c/bigest_number.c
+++ C workshop/Non-function c/bigest_number.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-int main(){
-  int n1,n2,res;
+int main()
+{
+  int n1,n2,result;
   n1=200;
   n2=300;
-  res=n1>n2?n1:n2;
-  printf("%d",res);
+  result=n1>n2?n1:n2;
+  printf("%d\n",result);
   return 0;
 }
