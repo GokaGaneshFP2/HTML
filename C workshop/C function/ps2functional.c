e8b477fc5c004b5eee0aeea91b25b8c48e8d5193
G.Ganesh <158814955+GokaGaneshFP2@users.noreply.github.com>
2024-02-12 13:39:03 +0530

    Update ps2functional.c

--- C workshop/C function/ps2functional.c
+++ C workshop/C function/ps2functional.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-void dosquare(int a,int b){
+void dosquare(int a,int b)
+{
     int result;
     result=(a*a)+(b*b)+(2*(a*b));
    printf("%d\n",result);
 }
-int main(){
+int main()
+{
     dosquare(10,20);
     dosquare(50,60);
     dosquare(30,20);
