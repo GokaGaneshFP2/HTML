240c334cc1fcd475c3d91162d369321e992ebd5f
G.Ganesh <158814955+GokaGaneshFP2@users.noreply.github.com>
2024-02-12 13:37:04 +0530

    Update biggestof2.c

--- C workshop/C function/ps4functional.c
+++ C workshop/C function/ps4functional.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-void dobiggest(int n1,int n2){
-    int res;
-    res=n1>n2?n1:n2;
-    printf("%d \n",res);
+void dobiggest(int n1,int n2)
+{
+    int result;
+    result=n1>n2?n1:n2;
+    printf("%d\n",result);
 }
-int main(){
+int main()
+{
     dobiggest(100,250);
     dobiggest(300,50);
     dobiggest(1000,2500);
