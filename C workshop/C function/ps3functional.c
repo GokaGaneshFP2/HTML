28ae8fc043b6af6c10ad3e87e4fced48be0f24ab
G.Ganesh <158814955+GokaGaneshFP2@users.noreply.github.com>
2024-02-12 13:38:38 +0530

    Update ps3functional.c

--- C workshop/C function/ps3functional.c
+++ C workshop/C function/ps3functional.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-void doaverage(int a,int b,int c){
+void doaverage(int a,int b,int c)
+{
     int result;
     result=(a+b+c)/3;
-    printf("%d \n",result);
+    printf("%d\n",result);
 }
-int main(){
+int main()
+{
     doaverage(10,20,30);
     doaverage(40,200,340);
     doaverage(400,100,930);
